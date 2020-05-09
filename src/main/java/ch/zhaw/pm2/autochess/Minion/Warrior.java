7621fcd5fd42b4d7b2aa341cd1971122ac04b885
Alex Schwarz <schwaal3@students.zhaw.ch>
2020-05-09 22:18:53 +0200

    Adjustments for Merge conflict between model-implementation and boardManager-implementation. Added minionException to relevant methods in Hero and Game. Adjusted MinionBase to hold both attack- and movementRange wit getter and setter. Adapted tests to changes.

--- src/main/java/ch/zhaw/pm2/autochess/Minion/Warrior.java
+++ src/main/java/ch/zhaw/pm2/autochess/Minion/Warrior.java
@@ -1,9 +1,10 @@
 package ch.zhaw.pm2.autochess.Minion;
 
 import ch.zhaw.pm2.autochess.Board.MoveStrategy;
+import ch.zhaw.pm2.autochess.Minion.exceptions.MinionException;
 
 public class Warrior extends MinionBase {
-    public Warrior(int heroId) throws MinionException{
+    public Warrior(int heroId) throws MinionException {
         super(MinionType.WARRIOR, MoveStrategy.StrategyType.AGGRESSIVE, 20, 7, 4, 1,2, 3, heroId);
     }
 
