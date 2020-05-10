6fe9c7b706fdd4e09d88955dfd3f7f1337bd2a33
Alain Michienzi <michiala@students.zhaw.ch>
2020-05-11 01:11:00 +0200

    minor adjustments

--- src/main/java/ch/zhaw/pm2/autochess/MainApp.java
+++ src/main/java/ch/zhaw/pm2/autochess/MainApp.java
@@ -5,12 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainApp extends Application {
     private static final Logger logger = Logger.getLogger(MainApp.class.getCanonicalName());
+    private Stage primaryStage;
+
+    private static final int COLUMNS = 8;
+    private static final int ROWS = 8;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -21,17 +25,19 @@ public class MainApp extends Application {
         startNewGame(primaryStage);
     }
 
-    private void startNewGame(Stage primaryStage){
+    private void startNewGame(Stage primaryStage) {
+        this.primaryStage = primaryStage;
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("MainGameWindow.fxml"));
             Pane rootPane = loader.load();
-            // fill in scene and stage setup
+            rootPane.getStylesheets().add(String.valueOf(getClass().getResource("Stylesheet.css")));
+            rootPane.setId("pane");
             Scene scene = new Scene(rootPane);
 
-            // configure and show stage
+            primaryStage.setResizable(false);
             primaryStage.setScene(scene);
-            primaryStage.setMinWidth(420);
-            primaryStage.setMinHeight(250);
+            primaryStage.setMinWidth(720);
+            primaryStage.setMinHeight(900);
             primaryStage.setTitle("SMAC");
             primaryStage.show();
         } catch(Exception e) {
@@ -39,4 +45,3 @@ public class MainApp extends Application {
         }
     }
 }
-
