89d407bfed6a18e4c6c3d14d90aa3956ab650c9f
Austin Albrecht <austin.aa@me.com>
2020-05-14 22:32:55 +0200

    Add CowardStrategy
    Relevant issue: #39

--- src/main/java/ch/zhaw/pm2/autochess/Minion/strategy/CowardStrategy.java
+++ src/main/java/ch/zhaw/pm2/autochess/Minion/strategy/CowardStrategy.java
@@ -3,22 +3,80 @@ package ch.zhaw.pm2.autochess.Minion.strategy;
 import ch.zhaw.pm2.autochess.Minion.MinionBase;
 import ch.zhaw.pm2.autochess.PositionVector;
 
+import java.util.List;
+
 public class CowardStrategy extends MoveStrategy {
+
+    private static final int ACTIVATION_RANGE = 2;
+
+    /**
+     * Searches for nearby enemy minions in activation range. If it finds any it considers the closest minion the biggest threat
+     * and will try run as far away from it as possible.
+     *
+     * @param board    Current board state.
+     * @param position The position that the movement should be based on.
+     * @param self     The minion being controlled by the strategy.
+     * @return PositionVector of where the strategy wants to move. May return null for no movement.
+     */
     @Override
-    public PositionVector move(MinionBase[][] board, PositionVector position, int movementRange) {
-        if(board[position.getY()][position.getX() +1] == null) {
-            return new PositionVector(1,0);
-        } else {
-            return null;
+    public PositionVector move(MinionBase[][] board, PositionVector position, MinionBase self) {
+
+        PositionVector targetPosition = null;
+
+        List<PositionVector> enemies = findPositionsInRange(getNonFriendlyPositions(board, self), position, ACTIVATION_RANGE);
+        double shortest = 0.0;
+        PositionVector closestEnemy = null;
+        for (PositionVector possibleTarget : enemies) {
+            double distance = calculateDistance(possibleTarget, position);
+            if (shortest == 0.0 || distance < shortest) {
+                shortest = distance;
+                closestEnemy = possibleTarget;
+            }
         }
+
+
+        double longest = 0.0;
+        List<PositionVector> possiblePositions = findPositionsInRange(getUnoccupiedSpaces(board), position, self.getMovementRange());
+        if (closestEnemy != null) {
+            for (PositionVector possiblePosition : possiblePositions) {
+                double distance = calculateDistance(possiblePosition, closestEnemy);
+                if (distance > longest) {
+                    longest = distance;
+                    targetPosition = possiblePosition;
+                }
+            }
+        }
+
+        return targetPosition;
     }
 
+    /**
+     * Will always attack the closest minion within attack range.
+     * Same as the aggressive strategy.
+     *
+     * @param board    Current board state.
+     * @param position The position that the movement should be based on.
+     * @param self     The minion being controlled by the strategy.
+     * @return PositionVector of the minion that the strategy wants to attack. May return null for no attack.
+     */
     @Override
-    public PositionVector attack(MinionBase[][] board, PositionVector position, int movementRange) {
-        if(board[position.getY()][position.getX()+1] == null) {
-            return null;
-        } else {
-            return new PositionVector(6,3);
+    public PositionVector attack(MinionBase[][] board, PositionVector position, MinionBase self) {
+        // TODO: Deal with duplication
+        PositionVector targetPosition = null;
+
+        List<PositionVector> possibleTargets = findPositionsInRange(getNonFriendlyPositions(board, self), position, self.getAttackRange());
+
+        double shortest = 0.0;
+        if (!possibleTargets.isEmpty()) {
+            for (PositionVector possibleTarget : possibleTargets) {
+                double distance = calculateDistance(possibleTarget, position);
+                if (shortest == 0.0 || distance < shortest) {
+                    shortest = distance;
+                    targetPosition = possibleTarget;
+                }
+            }
         }
+
+        return targetPosition;
     }
 }
