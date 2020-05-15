78f5758ba6e843d68dd832ad4ec3e35e551d8083
Austin Albrecht <austin.aa@me.com>
2020-05-15 18:11:06 +0200

    Refactor CowardStrategy test as per comment in #63

--- src/test/java/ch/zhaw/pm2/autochess/CowardStrategyTest.java
+++ src/test/java/ch/zhaw/pm2/autochess/CowardStrategyTest.java
@@ -19,10 +19,20 @@ public class CowardStrategyTest {
     @Mock private MinionBase dummyMinion;
     private CowardStrategy strategy;
 
+    private final static int HERO_ID_A = 0;
+    private final static int HERO_ID_B = 1;
+    private final static int COWARD_MINION_ATK_RANGE = 1;
+    private final static int COWARD_MINION_MOV_RANGE = 2;
+    private final static int COWARD_MINION_ID = 0;
+    private final static int DUMMY_MINION_ID = 1;
+    private final static int ROWS = 8;
+    private final static int COLS = 8;
+    private final static PositionVector TOP_LEFT = new PositionVector(0, 0);
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        board = new MinionBase[8][8];
+        board = new MinionBase[ROWS][COLS];
 
         strategy = new CowardStrategy();
     }
@@ -33,7 +43,6 @@ public class CowardStrategyTest {
         setUpCowardMinionMock();
 
         PositionVector resultPos = strategy.move(board, new PositionVector(4, 4), cowardMinion);
-
         assertNull(resultPos);
     }
 
@@ -61,7 +70,7 @@ public class CowardStrategyTest {
         board[0][0] = cowardMinion;
         setUpCowardMinionMock();
 
-        PositionVector resultPos = strategy.attack(board, new PositionVector(0, 0), cowardMinion);
+        PositionVector resultPos = strategy.attack(board, TOP_LEFT, cowardMinion);
         assertNull(resultPos);
     }
 
@@ -73,11 +82,11 @@ public class CowardStrategyTest {
         board[1][1] = dummyMinion;
         setUpDummyMinionMock();
 
-        PositionVector resultPos = strategy.attack(board, new PositionVector(0, 0), cowardMinion);
+        PositionVector resultPos = strategy.attack(board, TOP_LEFT, cowardMinion);
         assertEquals(new PositionVector(1, 1), resultPos);
 
         board[1][0] = dummyMinion;
-        resultPos = strategy.attack(board, new PositionVector(0, 0), cowardMinion);
+        resultPos = strategy.attack(board, TOP_LEFT, cowardMinion);
         assertEquals(new PositionVector(0, 1), resultPos);
     }
 
@@ -90,14 +99,14 @@ public class CowardStrategyTest {
 
 
     private void setUpCowardMinionMock() {
-        when(cowardMinion.getMovementRange()).thenReturn(2);
-        when(cowardMinion.getAttackRange()).thenReturn(1);
-        when(cowardMinion.getId()).thenReturn(0);
-        when(cowardMinion.getHeroId()).thenReturn(0);
+        when(cowardMinion.getMovementRange()).thenReturn(COWARD_MINION_MOV_RANGE);
+        when(cowardMinion.getAttackRange()).thenReturn(COWARD_MINION_ATK_RANGE);
+        when(cowardMinion.getId()).thenReturn(COWARD_MINION_ID);
+        when(cowardMinion.getHeroId()).thenReturn(HERO_ID_A);
     }
 
     private void setUpDummyMinionMock() {
-        when(dummyMinion.getId()).thenReturn(2);
-        when(dummyMinion.getHeroId()).thenReturn(1);
+        when(dummyMinion.getId()).thenReturn(DUMMY_MINION_ID);
+        when(dummyMinion.getHeroId()).thenReturn(HERO_ID_B);
     }
 }
