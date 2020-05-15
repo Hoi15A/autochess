05e2a795c2f555483e6d9d918dbb7678c472096d
Alex Schwarz <schwaal3@students.zhaw.ch>
2020-05-15 23:41:28 +0200

    added java doc do game class

--- src/main/java/ch/zhaw/pm2/autochess/game/Game.java
+++ src/main/java/ch/zhaw/pm2/autochess/game/Game.java
@@ -28,6 +28,12 @@ public class Game {
     private final ArrayList<HeroBase> heroArrayList;
     private BoardManager boardManager;
 
+    /**
+     * Constructor for Game class. Initializes two hoeres of the given type and adds them the hero list.
+     * @param heroTypePlayer1 heroType for hero1
+     * @param heroTypePlayer2 heroType for hero2
+     * @throws IllegalGameStateException
+     */
     public Game(Config.HeroType heroTypePlayer1, Config.HeroType heroTypePlayer2) throws IllegalGameStateException {
         boardManager = new BoardManager();
         heroArrayList = new ArrayList<>();
@@ -41,6 +47,10 @@ public class Game {
         return heroArrayList;
     }
 
+    /**
+     * Getter method for the int value Hero ID winner of the game. If no winner returns -1.
+     * @return int hero ID value or -1 if no winner
+     */
     public int getWinner() {
         int winner = NO_WINNER;
         if(checkWinner()) {
@@ -99,15 +109,32 @@ public class Game {
         return foundHero;
     }
 
+    /**
+     * Getter method for funds attribute of hero matching given int ID value.
+     * @param heroId int ID value
+     * @return int funds value
+     * @throws IllegalGameStateException thrown if no hero matches the given ID
+     */
     public int getHeroFunds(int heroId) throws IllegalGameStateException {
         return getHero(heroId).getFunds();
     }
 
+    /**
+     * Getter method for health attribute of hero matching given int ID value.
+     * @param heroId int ID Value
+     * @return int health value
+     * @throws IllegalGameStateException thrown if no hero matches the given ID
+     */
     public int getHeroHealth(int heroId) throws IllegalGameStateException {
         return getHero(heroId).getHealth();
 
     }
 
+    /**
+     * Method to activate hero ability. Cannot only be activated once.
+     * @param heroId int ID value
+     * @throws IllegalGameStateException thrown if attempted modification not allowed or hero ID value not found
+     */
     public void doHeroAbility(int heroId) throws IllegalGameStateException {
         try {
             getHero(heroId).doAbility();
@@ -116,6 +143,12 @@ public class Game {
         }
     }
 
+    /**
+     * Getter method for hero ability status boolean. If ability has been activated, status is false;
+     * @param heroId int ID value
+     * @return boolean of ability status
+     * @throws IllegalGameStateException thrown if no hero matches the given ID
+     */
     public boolean getHeroAbilityStatus(int heroId) throws IllegalGameStateException {
         return getHero(heroId).isAbilityAvailable();
     }
@@ -124,6 +157,15 @@ public class Game {
     //Minion methods
     //***********************
 
+    /**
+     * Method to let hero matching given ID attempt to buy a minion of the given Type. Fails if type is invalid,
+     * price value invalid or funds reduction not allowed.
+     * If successful, adds minion of said type to hero minion list.
+     * @param heroId int ID value
+     * @param minionType {@link Config.MinionType} to buy
+     * @throws IllegalFundsReductionException thrown if funds not available
+     * @throws IllegalGameStateException thrown if internal values incorrect
+     */
     public void buyMinion(int heroId, Config.MinionType minionType) throws IllegalFundsReductionException, IllegalGameStateException {
         try {
             HeroBase hero = getHero(heroId);
@@ -133,6 +175,14 @@ public class Game {
         }
     }
 
+    /**
+     * Method to let hero matching given ID attempt to sell minion matching given ID.
+     * If successful, removes minion from hero minion list and increases funds.
+     * @param heroId
+     * @param minionId
+     * @throws IllegalGameStateException
+     * @throws InvalidMinionIDException
+     */
     public void sellMinion(int heroId, int minionId) throws IllegalGameStateException, InvalidMinionIDException {
         try {
             HeroBase hero = getHero(heroId);
@@ -142,6 +192,14 @@ public class Game {
         }
     }
 
+    /**
+     * Method to place minion matching given ID of hero matching given ID on the board at the given position.
+     * @param heroId int ID value
+     * @param minionId int ID value
+     * @param pos PositionVector of placement
+     * @throws InvalidPositionException thrown if position is not valid
+     * @throws IllegalGameStateException thrown if hero ID value matches no hero
+     */
     public void placeMinionOnBoard(int heroId, int minionId, PositionVector pos) throws InvalidPositionException, IllegalGameStateException {
         try {
             boardManager.placeMinionOnBoard(getHero(heroId).getMinion(minionId), pos);
@@ -151,14 +209,35 @@ public class Game {
 
     }
 
+    /**
+     * Getter method for minion info String.
+     * @param heroId int ID value
+     * @param minionId int ID value
+     * @return String info of minion
+     * @throws IllegalGameStateException thrown if hero ID value matches no hero
+     * @throws InvalidMinionIDException thrown if minion ID value matches no minion
+     */
     public String getMinionInfoAsString(int heroId, int minionId) throws IllegalGameStateException, InvalidMinionIDException {
             return getHero(heroId).getMinionInfoAsString(minionId);
     }
 
+    /**
+     * Getter method for list of int ID values of the hero matching the given ID.
+     * @param heroId int ID value
+     * @return Integer Set of all int minion IDs
+     * @throws IllegalGameStateException
+     */
     public Set<Integer> getAllMinionIds(int heroId) throws IllegalGameStateException {
         return getHero(heroId).getAllMinionIds();
     }
 
+    /**
+     * Getter method for minion type of minion matching given ID value from hero matching given ID value.
+     * @param heroId int ID value
+     * @param minionId int ID value
+     * @return {@link Config.MinionType} of minion
+     * @throws IllegalGameStateException
+     */
     public Config.MinionType getMinionType(int heroId, int minionId) throws IllegalGameStateException {
         try {
             return getHero(heroId).getMinionType(minionId);
@@ -171,14 +250,30 @@ public class Game {
     //BoardManager methods
     //*******************************
 
+    /**
+     * Method to remove the minion matching the given int ID value from the baord.
+     * @param minionId int ID value
+     * @throws MinionNotOnBoardException thrown if minion not on board
+     */
     public void removeMinionFromBoard(int minionId) throws MinionNotOnBoardException {
         boardManager.removeMinionFromBoard(minionId);
     }
 
+    /**
+     * Getter method for minion Position from board.
+     * @param minionId int ID value
+     * @return PositonVector of minion
+     * @throws MinionNotOnBoardException thrown if minion not on board
+     */
     public PositionVector getMinionPos(int minionId) throws MinionNotOnBoardException {
         return boardManager.getMinionPosition(minionId);
     }
 
+    /**
+     * Method to do a battle with the minions placed on the board. After battle calculates damage,
+     * distributes funds, resets minion health and clears the board of minion.
+     * @throws IllegalGameStateException thrown if internal values invalid
+     */
     public void doBattle() throws IllegalGameStateException {
         try {
             boardManager.clearBattleLogs();
@@ -217,6 +312,10 @@ public class Game {
         }
     }
 
+    /**
+     * Getter method for list of BattleLog objects generated form battle.
+     * @return List of BattleLog objects
+     */
     public List<BattleLog> getBattleLog() {
         return boardManager.getBattleLogs();
     }
