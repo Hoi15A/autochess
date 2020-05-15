49f86b15789941e92c37eb9d5be07b4e802f3e20
AlainMi95 <michiala@students.zhaw.ch>
2020-05-15 14:39:21 +0200

    -Implemented: textfield -> integer only -Added: Battle button

--- src/main/java/ch/zhaw/pm2/autochess/controller/GameController.java
+++ src/main/java/ch/zhaw/pm2/autochess/controller/GameController.java
@@ -11,6 +11,7 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -50,6 +51,16 @@ public class GameController implements Initializable {
     private Button p2PlaceButton;
     private GridPane p2minionplacegrid;
 
+    private TextField p1minionidField;
+    private Button p1removeButton;
+    private GridPane p1minionremovegrid;
+
+    private TextField p2minionidField;
+    private Button p2removeButton;
+    private GridPane p2minionremovegrid;
+
+    private Button battleButton;
+
     private Game game;
 
     @FXML
@@ -76,6 +87,8 @@ public class GameController implements Initializable {
            after battle -> getWinner() if winner(1 or 2) -> game over(back to main menu)  else(-1) -> open shop
          */
 
+        battleButton = new Button("Battle");
+
         initializeFieldGrid();
         initializePlayerGrid();
         initializeMinionList();
@@ -83,16 +96,96 @@ public class GameController implements Initializable {
         initializeRemoveMinionSection();
 
         player1Grid.add(p1minionplacegrid,0,1);
+        player1Grid.add(p1minionremovegrid,0,2);
+
         player2Grid.add(p2minionplacegrid,0,1);
+        player2Grid.add(p2minionremovegrid,0,2);
 
         gameMainGrid.add(fieldGrid, 1, 0);
         gameMainGrid.add(player1Grid, 0, 0);
         gameMainGrid.add(player2Grid, 2, 0);
+        gameMainGrid.add(battleButton,1,1);
     }
 
     private void initializeRemoveMinionSection() {
+        p1minionidField = new TextField();
+        p1removeButton = new Button("remove");
 
-    }
+        p1minionremovegrid = new GridPane();
+
+        for (int i = 0; i < 2; i++) {
+            ColumnConstraints column = new ColumnConstraints(75);
+            p1minionremovegrid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < 1; i++) {
+            RowConstraints row = new RowConstraints(100);
+            p1minionremovegrid.getRowConstraints().add(row);
+        }
+
+        p1minionremovegrid.add(p1minionidField,0,0);
+        p1minionremovegrid.add(p1removeButton,1,0);
+
+        p1minionidField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p1minionidField.getText().matches("\\d*")) {
+                p1minionidField.setText(p1minionidField.getText().replaceAll("[^\\d]", ""));
+            }
+        });
+
+        p1removeButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+             //   int id = Integer.parseInt(p1minionidField.getText());
+                refreshGameGrid();
+
+             /*   try {
+                    game.removeMinionFromBoard(id);
+                    //todo   fieldGrid.getChildren().remove();
+                } catch (MinionNotOnBoardException e) {
+                    e.printStackTrace();
+                }*/
+            }
+            });
+
+        p2minionidField = new TextField();
+        p2removeButton = new Button("remove");
+
+        p2minionremovegrid = new GridPane();
+
+        for (int i = 0; i < 2; i++) {
+            ColumnConstraints column = new ColumnConstraints(75);
+            p2minionremovegrid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < 1; i++) {
+            RowConstraints row = new RowConstraints(100);
+            p2minionremovegrid.getRowConstraints().add(row);
+        }
+
+        p2minionremovegrid.add(p2minionidField,0,0);
+        p2minionremovegrid.add(p2removeButton,1,0);
+
+        p2minionidField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p2minionidField.getText().matches("\\d*")) {
+                p2minionidField.setText(p2minionidField.getText().replaceAll("[^\\d]", ""));
+            }
+        });
+
+        p2removeButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //   int id = Integer.parseInt(p1minionidField.getText());
+                refreshGameGrid();
+
+             /*   try {
+                    game.removeMinionFromBoard(id);
+                    //todo   fieldGrid.getChildren().remove();
+                } catch (MinionNotOnBoardException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
+        }
 
     private void initializePlaceMinionSection() {
         p1minionIdTF = new TextField();
@@ -112,10 +205,28 @@ public class GameController implements Initializable {
             p1minionplacegrid.getRowConstraints().add(row);
         }
 
-        p1minionplacegrid.add(p1fieldposXTF,0,0);
-        p1minionplacegrid.add(p1fieldposYTF,1,0);
-        p1minionplacegrid.add(p1minionIdTF,0,1);
-        p1minionplacegrid.add(p1PlaceButton,1,1);
+        p1minionplacegrid.add(p1fieldposXTF, 0, 0);
+        p1minionplacegrid.add(p1fieldposYTF, 1, 0);
+        p1minionplacegrid.add(p1minionIdTF, 0, 1);
+        p1minionplacegrid.add(p1PlaceButton, 1, 1);
+
+        p1minionIdTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p1minionIdTF.getText().matches("\\d*")) {
+                p1minionIdTF.setText(p1minionIdTF.getText().replaceAll("[^\\d]", ""));
+            }
+        });
+
+        p1fieldposXTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p1fieldposXTF.getText().matches("\\d*")) {
+                p1fieldposXTF.setText(p1fieldposXTF.getText().replaceAll("[^\\d]", ""));
+            }
+        });
+
+        p1fieldposYTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p1fieldposYTF.getText().matches("\\d*")) {
+                p1fieldposYTF.setText(p1fieldposYTF.getText().replaceAll("[^\\d]", ""));
+            }
+        });
 
         p1PlaceButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -123,14 +234,14 @@ public class GameController implements Initializable {
                 int id = Integer.parseInt(p1minionIdTF.getText());
                 int x = Integer.parseInt(p1fieldposXTF.getText());
                 int y = Integer.parseInt(p1fieldposYTF.getText());
-                System.out.println("id: " + id+ " x: " + x+ " y: " +y);
+                System.out.println("id: " + id + " x: " + x + " y: " + y);
                 PositionVector posVector = new PositionVector();
                 posVector.setX(x);
                 posVector.setY(y);
 
                 try {
                     game.placeMinionOnBoard(1, id, posVector);
-                    fieldGrid.add(new Text("test"),posVector.getX(),posVector.getY());
+                    fieldGrid.add(new Text("test"), posVector.getX(), posVector.getY());
                 } catch (InvalidPositionException e) {
                     e.printStackTrace();
                 } catch (IllegalGameStateException e) {
@@ -139,54 +250,69 @@ public class GameController implements Initializable {
             }
         });
 
-            p2minionIdTF = new TextField();
-            p2fieldposXTF = new TextField();
-            p2fieldposYTF = new TextField();
-            p2PlaceButton = new Button("place");
+        p2minionIdTF = new TextField();
+        p2fieldposXTF = new TextField();
+        p2fieldposYTF = new TextField();
+        p2PlaceButton = new Button("place");
 
-            p2minionplacegrid = new GridPane();
+        p2minionplacegrid = new GridPane();
 
-            for (int i = 0; i < 2; i++) {
-                ColumnConstraints column = new ColumnConstraints(75);
-                p2minionplacegrid.getColumnConstraints().add(column);
+        for (int i = 0; i < 2; i++) {
+            ColumnConstraints column = new ColumnConstraints(75);
+            p2minionplacegrid.getColumnConstraints().add(column);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            RowConstraints row = new RowConstraints(50);
+            p2minionplacegrid.getRowConstraints().add(row);
+        }
+
+        p2minionplacegrid.add(p2fieldposXTF, 0, 0);
+        p2minionplacegrid.add(p2fieldposYTF, 1, 0);
+        p2minionplacegrid.add(p2minionIdTF, 0, 1);
+        p2minionplacegrid.add(p2PlaceButton, 1, 1);
+
+        p2minionIdTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p2minionIdTF.getText().matches("\\d*")) {
+                p2minionIdTF.setText(p2minionIdTF.getText().replaceAll("[^\\d]", ""));
             }
+        });
 
-            for (int i = 0; i < 2; i++) {
-                RowConstraints row = new RowConstraints(50);
-                p2minionplacegrid.getRowConstraints().add(row);
+        p2fieldposXTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p2fieldposXTF.getText().matches("\\d*")) {
+                p2fieldposXTF.setText(p2fieldposXTF.getText().replaceAll("[^\\d]", ""));
             }
+        });
 
-        p2minionplacegrid.add(p2fieldposXTF,0,0);
-        p2minionplacegrid.add(p2fieldposYTF,1,0);
-        p2minionplacegrid.add(p2minionIdTF,0,1);
-        p2minionplacegrid.add(p2PlaceButton,1,1);
+        p2fieldposYTF.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!p2fieldposYTF.getText().matches("\\d*")) {
+                p2fieldposYTF.setText(p2fieldposYTF.getText().replaceAll("[^\\d]", ""));
+            }
+        });
 
-            p2PlaceButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    int id = Integer.parseInt(p2minionIdTF.getText());
-                    int x = Integer.parseInt(p2fieldposXTF.getText());
-                    int y = Integer.parseInt(p2fieldposYTF.getText());
-                    System.out.println("id: " + id+ " x: " + x+ " y: " +y);
-                    PositionVector posVector = new PositionVector();
-                    posVector.setX(x);
-                    posVector.setY(y);
+        p2PlaceButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                int id = Integer.parseInt(p2minionIdTF.getText());
+                int x = Integer.parseInt(p2fieldposXTF.getText());
+                int y = Integer.parseInt(p2fieldposYTF.getText());
+                System.out.println("id: " + id + " x: " + x + " y: " + y);
+                PositionVector posVector = new PositionVector();
+                posVector.setX(x);
+                posVector.setY(y);
 
-                    try {
-                        game.placeMinionOnBoard(2, id, posVector);
-                        fieldGrid.add(new Text("test"),posVector.getX(),posVector.getY());
-                    } catch (InvalidPositionException e) {
-                        e.printStackTrace();
-                    } catch (IllegalGameStateException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    game.placeMinionOnBoard(2, id, posVector);
+                    fieldGrid.add(new Text("test"), posVector.getX(), posVector.getY());
+                } catch (InvalidPositionException e) {
+                    e.printStackTrace();
+                } catch (IllegalGameStateException e) {
+                    e.printStackTrace();
                 }
-            });
-
-
-
-
+            }
+        });
     }
+
 
     private void initializeFieldGrid() {
         fieldGrid = new GridPane();
@@ -273,5 +399,9 @@ public class GameController implements Initializable {
         } catch (InvalidMinionIDException e) {
             e.printStackTrace();
         }
+    }
+
+    private void refreshGameGrid(){
+
     }
 }
